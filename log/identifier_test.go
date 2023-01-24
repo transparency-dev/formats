f05a1ae71e8ac5b705f6688080a9384d834faaff
Martin Hutchinson <mhutchinson@gmail.com>
2023-01-24 10:29:05 +0000

    Added more tests to demonstrate that both origin and key are used in the id generation

--- log/identifier_test.go
+++ log/identifier_test.go
@@ -39,6 +39,24 @@ func TestID(t *testing.T) {
 			pk:     []byte("armory-drive-log+16541b8f+AYDPmG5pQp4Bgu0a1mr5uDZ196+t8lIVIfWQSPWmP+Jv"),
 			want:   "4ad2fce3c699522bf81ef663bdc973e83a27ea1b8c92ea6d030747d73cd9dae8",
 		},
+		{
+			desc:   "rekor 1",
+			origin: "rekor.sigstore.dev - 2605736670972794746",
+			pk:     []byte("rekor.sigstore.dev+c0d23d6a+AjBZMBMGByqGSM49AgEGCCqGSM49AwEHA0IABNhtmPtrWm3U1eQXBogSMdGvXwBcK5AW5i0hrZLOC96l+smGNM7nwZ4QvFK/4sueRoVj//QP22Ni4Qt9DPfkWLc="),
+			want:   "afc7cd5295b43e3d95c4058b9e115b3760c71b9cd2f5c3104e121bd9970359b9",
+		},
+		{
+			desc:   "rekor 2",
+			origin: "rekor.sigstore.dev - 3904496407287907110",
+			pk:     []byte("rekor.sigstore.dev+c0d23d6a+AjBZMBMGByqGSM49AgEGCCqGSM49AwEHA0IABNhtmPtrWm3U1eQXBogSMdGvXwBcK5AW5i0hrZLOC96l+smGNM7nwZ4QvFK/4sueRoVj//QP22Ni4Qt9DPfkWLc="),
+			want:   "5be31cffff16384a6f6fa895b2bf3bb510d7b59197fb488e6e0aaf9d1b72d9ad",
+		},
+		{
+			desc:   "rekor 3",
+			origin: "rekor.sigstore.dev - 3904496407287907110",
+			pk:     []byte("armory-drive-log+16541b8f+AYDPmG5pQp4Bgu0a1mr5uDZ196+t8lIVIfWQSPWmP+Jv"),
+			want:   "750e9a8dc89eb1334fc53db4d8feb94acf4e45c2e7f5dd00d08c665e6fbc8483",
+		},
 	} {
 		t.Run(test.desc, func(t *testing.T) {
 			if got, want := log.ID(test.origin, test.pk), test.want; got != want {
