429abf782964fba67cffe1acb69ee57abcef7936
Al Cutter <al@google.com>
2026-08-04 17:28:48 +0100

    Address comments

--- note/note.go
+++ note/note.go
@@ -57,7 +57,7 @@ func NewSigner(skey string) (Signer, error) {
 	case algEd25519:
 		// We "re-implement" this here so as to be able to return a local Signer instance (as
 		// opposed to a note.Signer).
-		// This has the benefit that all signers from this packge carry a Verifier() getter.
+		// This has the benefit that all signers from this package carry a Verifier() getter.
 
 		// Adapted from sumdb/note/note.go
 		hash, err := strconv.ParseUint(hash16, 16, 32)
@@ -96,32 +96,6 @@ func NewSigner(skey string) (Signer, error) {
 	}
 }
 
-// NewVerifier returns a verifier for the given key, if the key's algo is known.
-func NewVerifier(key string) (note.Verifier, error) {
-	parts := strings.SplitN(key, "+", 3)
-	if got, want := len(parts), 3; got != want {
-		return nil, fmt.Errorf("key has %d parts, expected %d: %q", got, want, key)
-	}
-	keyBytes, err := base64.StdEncoding.DecodeString(parts[2])
-	if err != nil {
-		return nil, fmt.Errorf("key has invalid base64 %q: %v", parts[2], err)
-	}
-	if len(keyBytes) < 2 {
-		return nil, fmt.Errorf("invalid key, key bytes too short")
-	}
-
-	switch keyBytes[0] {
-	case algECDSAWithSHA256:
-		return NewECDSAVerifier(key)
-	case algEd25519CosignatureV1, algMLDSA44:
-		return NewVerifierForCosignatureV1(key)
-	case algRFC6962STH:
-		return NewRFC6962Verifier(key)
-	default:
-		return note.NewVerifier(key)
-	}
-}
-
 // The structs below are used by most/all of the signers and verifiers in this package.
 
 // Signer is a note.Signer which also provides access to the corresponding Verifier.
@@ -147,6 +121,32 @@ func (s *signer) Verifier() note.Verifier {
 		name:    s.name,
 		keyHash: s.hash,
 		v:       s.verify,
+	}
+}
+
+// NewVerifier returns a verifier for the given key, if the key's algo is known.
+func NewVerifier(key string) (note.Verifier, error) {
+	parts := strings.SplitN(key, "+", 3)
+	if got, want := len(parts), 3; got != want {
+		return nil, fmt.Errorf("key has %d parts, expected %d: %q", got, want, key)
+	}
+	keyBytes, err := base64.StdEncoding.DecodeString(parts[2])
+	if err != nil {
+		return nil, fmt.Errorf("key has invalid base64 %q: %v", parts[2], err)
+	}
+	if len(keyBytes) < 2 {
+		return nil, fmt.Errorf("invalid key, key bytes too short")
+	}
+
+	switch keyBytes[0] {
+	case algECDSAWithSHA256:
+		return NewECDSAVerifier(key)
+	case algEd25519CosignatureV1, algMLDSA44:
+		return NewVerifierForCosignatureV1(key)
+	case algRFC6962STH:
+		return NewRFC6962Verifier(key)
+	default:
+		return note.NewVerifier(key)
 	}
 }
 
