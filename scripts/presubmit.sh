449439fa02bbf7451129bd36f865dab19981e11b
Martin Hutchinson <mhutchinson@gmail.com>
2026-09-07 09:54:45 +0000

    Bump tooling to support 1.26 as minimum

--- scripts/presubmit.sh
+++ scripts/presubmit.sh
@@ -106,7 +106,7 @@ main() {
       'have you installed github.com/golangci/golangci-lint?' || exit 1
 
     echo 'running golangci-lint'
-    golangci-lint run --deadline=8m
+    golangci-lint run --timeout=8m
     echo 'checking license headers'
     ./scripts/check_license.sh ${go_srcs}
   fi
